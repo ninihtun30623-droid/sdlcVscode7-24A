a47d41d7ac97a28ec3fa90214b779357a1ba1ac6
ninihtun30623 <ninithun30623@gmail.com>
2025-10-01 09:02:15 +0630

    Initial Setup Continuous Integration

---
+++ sdlcEclipse/src/sdlcEclipse/Hellosdlc.java
@@ -0,0 +1,13 @@
+package sdlcEclipse;
+
+public class Hellosdlc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Continous integration");
+			
+
+	}
+
+}
